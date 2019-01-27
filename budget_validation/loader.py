311cd5806365606ed2ed2d30d2e6476a4dfc5080
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-01-27 21:34:07 +0100

    use cache for api calls

--- budget_validation/loader.py
+++ budget_validation/loader.py
@@ -1,22 +1,27 @@
 from .auth import get_client
 from .utils import clean_currency
 import pandas as pd
+from cachier import cachier
 
 
+@cachier(pickle_reload=True)
 def get_spreadsheet():
     client = get_client()
     return client.open("saisie_db")
 
 
+@cachier()
 def get_worksheet(worksheet_name):
     spreadsheet = get_spreadsheet()
     return spreadsheet.worksheet_by_title(worksheet_name)
 
 
+@cachier()
 def get_worksheet_as_df(worksheet_name):
     return pd.DataFrame(get_worksheet(worksheet_name).get_all_records())
 
 
+@cachier()
 def load_budget():
     """
     Return a dataframe for budget validation
