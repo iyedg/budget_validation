67de98fa377c5be4b7cc369b4996413ad42d01be
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-02 15:16:18 +0100

    remove matplotlib util functions

--- budget_validation/utils.py
+++ budget_validation/utils.py
@@ -5,7 +5,6 @@ import arabic_reshaper
 import numpy as np
 import pandas as pd
 from bidi.algorithm import get_display
-import matplotlib.pyplot as plt
 
 
 def clean_currency(col):
@@ -25,20 +24,3 @@ def list_to_dropdown_options(lst):
 def reshape(string):
     return get_display(arabic_reshaper.reshape(string))
 
-
-# from https://github.com/4QuantOSS/DashIntro/blob/master/notebooks/Tutorial.ipynb
-def fig_to_uri(in_fig, close_all=True, **save_args):
-    # type: (plt.Figure) -> str
-    """
-    Save a figure as a URI
-    :param in_fig:
-    :return:
-    """
-    out_img = BytesIO()
-    in_fig.savefig(out_img, format="png", **save_args)
-    if close_all:
-        in_fig.clf()
-        plt.close("all")
-    out_img.seek(0)  # rewind file
-    encoded = base64.b64encode(out_img.read()).decode("ascii").replace("\n", "")
-    return "data:image/png;base64,{}".format(encoded)
