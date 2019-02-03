d18d65cdf5aba804c99c12fae6ae32420ba26cf9
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-03 21:14:38 +0100

    Dark table style

--- budget_validation/dashboard/layout.py
+++ budget_validation/dashboard/layout.py
@@ -36,6 +36,9 @@ def get_datatable(df):
         columns=[{"name": i, "id": i} for i in df.columns],
         data=df.to_dict("rows"),
         style_table={"overflowX": "scroll"},
+        style_header={"backgroundColor": "rgb(30, 30, 30)"},
+        style_cell={"backgroundColor": "rgb(50, 50, 50)", "color": "white"},
+        style_as_list_view=True,
     )
     return datatable
 
