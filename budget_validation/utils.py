c7de94ebc2d86c2625f9dbc4de58c48f1010e6c1
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-09 09:43:59 +0100

    clean_currency for single values instead of dataframe columns

--- budget_validation/utils.py
+++ budget_validation/utils.py
@@ -1,18 +1,15 @@
-import base64
-from io import BytesIO
-
 import arabic_reshaper
 import numpy as np
 import pandas as pd
 from bidi.algorithm import get_display
 
 
-def clean_currency(col):
-    ill_formatted_numbers = col.apply(lambda val: "TND" not in str(val))
-    col.loc[ill_formatted_numbers].apply(lambda val: val.replace(",", "."))
-    return pd.to_numeric(col.str.replace("TND", "").str.replace(",", "")).apply(
-        lambda val: np.round(val, 3)
-    )
+def clean_currency(string):
+    if "TND" in string:
+        currency_string = string.replace("TND", "").replace(",", "")
+    else:
+        currency_string = string.replace(",", ".")
+    return np.round(pd.to_numeric(currency_string, errors="coerce"), 3)
 
 
 def list_to_dropdown_options(lst):
@@ -23,4 +20,3 @@ def list_to_dropdown_options(lst):
 
 def reshape(string):
     return get_display(arabic_reshaper.reshape(string))
-
