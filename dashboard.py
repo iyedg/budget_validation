3b670f73026ae6b319a2b02a88121b134cddad9f
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-09 10:11:10 +0100

    Replace load_budget  with the DRYer merged_table

--- dashboard.py
+++ dashboard.py
@@ -12,18 +12,25 @@ from budget_validation.dashboard.layout import (
     get_organization_name_dropdown,
     get_year_dropdown,
 )
-from budget_validation.loader import get_worksheet_as_df
+from budget_validation.loader import get_worksheet_as_df, merged_table
 from budget_validation.tree import draw_tree
 from budget_validation.utils import clean_currency, list_to_dropdown_options
 
 
-def get_budget_df():
-    """
-    Return a dataframe for budget validation
-    TODO: improve API, make it DRY and general
-    """
+budget_type = get_worksheet_as_df("budget_type")
+budget_by_type = get_worksheet_as_df("budget_by_type")
 
-    drop_cols = [
+budget = merged_table(
+    hierarchy_df=budget_type,
+    hierarchy_df_on="name",
+    values_df=budget_by_type,
+    values_df_on="budget_type_name",
+    transformers={
+        "value": [clean_currency],
+        "budget_type_parent_name": [lambda val: np.nan if val == "" else val.strip()],
+        "budget_type_name": [lambda val: np.nan if val == "" else val.strip()],
+    },
+    drop_cols=[
         "name_fr",
         "name_en",
         "name_ar",
@@ -34,37 +41,10 @@ def get_budget_df():
         "id_y",
         "description",
         "name",
-    ]
-    budget_type = get_worksheet_as_df("budget_type")
-    budget_by_type = get_worksheet_as_df("budget_by_type")
-    budget = (
-        pd.merge(
-            left=budget_type,
-            left_on="name",
-            right=budget_by_type,
-            right_on="budget_type_name",
-        )
-        .drop(columns=drop_cols)
-        .rename(columns={"parent_name": "budget_type_parent_name"})
-        .pipe(
-            lambda df: df.assign(
-                budget_type_parent_name=df["budget_type_parent_name"]
-                .str.strip()
-                .replace("", np.nan)
-            )
-        )
-        .pipe(lambda df: df.assign(value=df["value"].str.strip()))
-        .pipe(
-            lambda df: df.assign(organization_name=df["organization_name"].str.strip())
-        )
-        .pipe(lambda df: df.assign(budget_type_name=df["budget_type_name"].str.strip()))
-        .dropna(subset=["value"])
-        .pipe(lambda df: df.assign(value=clean_currency(df.value)))
-    )
-    return budget
+    ],
+    rename_cols={"parent_name": "budget_type_parent_name"},
+)
 
-
-budget = get_budget_df()
 
 app = dash.Dash(__name__)
 app.css.append_css({"external_url": "https://codepen.io/iyedg/pen/gqRLLM.css"})
