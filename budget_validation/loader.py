efe34a34822a141477bc2f42ef1b7077e90285b8
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-09 09:45:09 +0100

    function to merge a hierarchical df and a values df

--- budget_validation/loader.py
+++ budget_validation/loader.py
@@ -20,3 +20,43 @@ def get_worksheet(worksheet_name):
 def get_worksheet_as_df(worksheet_name):
     return pd.DataFrame(get_worksheet(worksheet_name).get_all_records())
 
+
+def merged_table(
+    hierarchy_df,
+    hierarchy_df_on,
+    values_df,
+    values_df_on,
+    transformers={},
+    drop_cols=[],
+    rename_cols={},
+):
+    """Link a values table to its corresponding hierarchy table
+
+    Arguments:
+        hierarchy_df {DataFrame} -- DataFrame containing the tree structure of the entity
+        hierarchy_df_on {str} -- The column on which the hierarchy DataFrame will occurr
+        values_df {DataFrame} -- DataFrame containing the values for entries in the hierarchy DataFrame
+        values_df_on {str} -- The column on which the values DataFrame will occurr
+        transformers {dict} -- A mapping of column names and a list functions to be applied to said column after the merger occurs
+        drop_cols {list} -- A list of column names to be dropped after the merger occurs
+        rename_cols {dict} -- A mapping of existing column names and their replacement values, renaming
+                              occurs after the merger occurs but before transformers are applied
+    """
+
+    # TODO transformers by dtype
+    merged = (
+        pd.merge(
+            left=hierarchy_df,
+            left_on=hierarchy_df_on,
+            right=values_df,
+            right_on=values_df_on,
+        )
+        .rename(columns=rename_cols)
+        .drop(columns=drop_cols)
+    )
+    for col_name, transformer_list in transformers.items():
+        for transformer in transformer_list:
+            merged.loc[:, col_name] = merged.loc[:, col_name].apply(transformer)
+            pass
+    return merged
+
