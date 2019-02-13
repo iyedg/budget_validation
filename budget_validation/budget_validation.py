31b669cf5d1ad835674a9a3078cbde00908f373d
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-02-13 09:16:41 +0100

    clear cache on load

--- budget_validation/budget_validation.py
+++ budget_validation/budget_validation.py
@@ -1,7 +1,9 @@
 from . import create_dash, create_flask
+from .loader import clear_cache
 
 SERVER = create_flask("../config.py")
 APP = create_dash(SERVER)
+clear_cache()
 
 with SERVER.app_context():
     from . import callbacks
